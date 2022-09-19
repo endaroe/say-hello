8bdf004db9c4a25ca0d8588622a96631a7bbd227
Imam Daru Nurwijayanto <Imamd@rspondokindah.co.id>
2022-09-19 10:26:30 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/endaroe/say-hello
+module github.com/endaroe/say-hello/v2
 
 go 1.19
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello Masbrow"
+func SayHello(name string) string {
+	return "Hello Masbrow " + name
 }
